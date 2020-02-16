8a3b9d3d55e6476ea90a257ba0aa746618ab191c
zBabEs <32611645+prapawit201@users.noreply.github.com>
2020-02-16 11:16:48 +0700

    [Day 2]ReactWorkshop

---
+++ ReactWorkshop/product-inventory/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ProductList extends React.Component {  //!copy ความสามารถมาจาก react เลยต้อง extends
+  render(){ //!ฟังชั่น
+    return (
+    <div>Product List</div>
+    )
+  }
+} 
+
+export default ProductList //!ต้องมีการ export ออกไปใช้ทุกครั้ง
