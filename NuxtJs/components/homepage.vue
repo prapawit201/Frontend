25edc2f5314352cd50c06ee5b55c4556e3a37148
zBabEs <32611645+prapawit201@users.noreply.github.com>
2021-03-28 00:24:07 +0700

    [UPDATE]update connect page

---
+++ NuxtJs/components/homepage.vue
@@ -0,0 +1,30 @@
+<template>
+<b-card
+    title="Card Title"
+    img-src="https://picsum.photos/600/300/?image=25"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      Some quick example text to build on the card title and make up the bulk of the card's content.
+    </b-card-text>
+     <b-button variant="outline-success" @click="nextPage()">Success</b-button>
+  </b-card>
+</template>
+
+<script>
+export default {
+    methods: {
+        nextPage(){
+            this.$router.push('/form')
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
