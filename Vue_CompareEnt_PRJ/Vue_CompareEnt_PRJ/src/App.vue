aaefd20e78ef2c807a2034dce2b2d239433eecc6
zBabEs <32611645+prapawit201@users.noreply.github.com>
2021-05-15 14:09:37 +0700

    inteoduction vue

---
+++ Vue_CompareEnt_PRJ/Vue_CompareEnt_PRJ/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    hi
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
