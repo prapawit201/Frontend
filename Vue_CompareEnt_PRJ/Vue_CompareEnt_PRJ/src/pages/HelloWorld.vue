83c40297daca9c68454ff479fbe47c329a5b48e5
zBabEs <32611645+prapawit201@users.noreply.github.com>
2021-05-15 15:17:50 +0700

    [UPDATE] update install bootstrap Vue

--- Vue_CompareEnt_PRJ/Vue_CompareEnt_PRJ/src/pages/HelloWorld.vue
+++ Vue_CompareEnt_PRJ/Vue_CompareEnt_PRJ/src/pages/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    Hello world
-      <b-button variant="info">Info</b-button>
+    Hello world <br>
+  <router-link to="/compare"><b-button variant="outline-success">clickme !</b-button></router-link>
   </div>
 </template>
 <script>
